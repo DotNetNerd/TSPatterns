3698f4ca040a369d88ab2f46a41c8faa996b047f
Christian Holm Nielsen <christian@dotnetnerd.dk>
2014-12-04 17:21:58 +0100

    Added singleton

---
+++ singleton.ts
@@ -0,0 +1,52 @@
+﻿/*
+Ensure that only one instance of a class is created
+Provide a global point of access to the object
+*/
+
+module Singleton {
+    
+    export class SingletonClass {
+
+        private static instance: SingletonClass = new SingletonClass();
+
+        private count: number = 0;
+
+        constructor() {
+            if (SingletonClass.instance) {
+                throw new Error("Error constructor should not be used directly. Use getInstance() instead.");
+            }
+            SingletonClass.instance = this;
+        }
+
+        public static getInstance(): SingletonClass {
+            if (SingletonClass.instance === null) {
+                SingletonClass.instance = new SingletonClass();
+            }
+            return SingletonClass.instance;
+        }
+
+        public setCount(value: number): void {
+            this.count = value;
+        }
+
+        public getCount(): number {
+            return this.count;
+        }
+
+        public add(value: number): void {
+            this.count += value;
+        }
+
+        public remove(value: number): void {
+            this.count -= value;
+        }
+
+    }
+}
+
+var counter = Singleton.SingletonClass.getInstance();
+var counter2 = Singleton.SingletonClass.getInstance();
+counter.setCount(20);
+counter2.add(5);
+counter.remove(15);
+write(counter.getCount());
