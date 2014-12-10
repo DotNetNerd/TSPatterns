ed8da5d8adc340cbdc8fbb8c16f952f6a7cb6f15
Christian Holm Nielsen <christian@dotnetnerd.dk>
2014-12-10 10:32:11 +0100

    Added strategy and builder

---
+++ builder.ts
@@ -0,0 +1,53 @@
+﻿/*
+Provide methods for building an object, rather than muitiple complex constructors
+Allows building complex objects in simpler steps
+*/
+
+module Builder {
+    export class CarBuilder {
+        name: string;
+        year: number;
+        make: string;
+        model: string;
+
+        constructor(name: string) {
+            this.name = name;
+        }
+        
+        setYear(value: number): CarBuilder {
+            this.year = value;
+            return this;
+        }
+
+        setModel(make: string, model: string): CarBuilder {
+            this.make = make;
+            this.model = model;
+            return this;
+        }
+        
+        build(): Car {
+            return new Car(this);
+        }
+    }
+
+    export class Car {
+        private name: string;
+        private year: number;
+        private make: string;
+        private model: string;
+
+        constructor(builder: CarBuilder) {
+            this.name = builder.name;
+            this.year = builder.year;
+            this.make = builder.make;
+            this.model = builder.model;
+        }
+    }
+}
+
+var myOldCar = new Builder.CarBuilder("Rusty")
+    .setModel("Skoda", "Fabia")
+    .setYear(2000)
+    .build();
+
+write(myOldCar);
---
+++ strategy.ts
@@ -0,0 +1,41 @@
+﻿/*
+Define a generic algorythm and make implementations interchangable
+Allows for flexibility in choise of strategy in given context
+*/
+
+module Strategy {
+    
+    export interface IStrategy {
+        execute(): void;
+    }
+
+    export class StrategyOne implements IStrategy {
+        public execute(): void {
+            write("StrategyOne executed");
+        }
+    }
+
+    export class StrategyTwo implements IStrategy {
+        public execute(): void {
+            write("StrategyTwo executed");
+        }
+    }
+    
+    export class Context {
+        private strategy: IStrategy;
+
+        constructor(strategy: IStrategy) {
+            this.strategy = strategy;
+        }
+
+        public executeStrategy(): void {
+            this.strategy.execute();
+        }
+    }
+}
+
+var context = new Strategy.Context(new Strategy.StrategyOne());
+context.executeStrategy();
+
+var otherContext = new Strategy.Context(new Strategy.StrategyTwo());
+otherContext.executeStrategy();
