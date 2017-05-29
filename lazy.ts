89c1be69f1a35683320f3a340b4203d826031752
Christian Holm Diget <christian@dotnetnerd.dk>
2017-05-29 13:58:23 +0200

    Added lazy

---
+++ lazy.ts
@@ -0,0 +1,20 @@
+﻿export interface ILazyInitializer<T> {
+    (): T
+}
+
+export class Lazy<T> {
+    private instance: T = null;
+    private initializer: ILazyInitializer<T>;
+
+    constructor(initializer: ILazyInitializer<T>) {
+        this.initializer = initializer;
+    }
+
+    public get Value(): T {
+        if (this.instance == null) {
+            this.instance = this.initializer();
+        }
+
+        return this.instance;
+    }
+}
